5e605691aec7087373c838a4ca26990c28cf619f
Herbert Huang <xh2305@columbia.edu>
2018-03-05 18:50:35 -0500

    new

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
 import requests
 import json
+from socket import *
+
+sock = socket()
+sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
 
 app = Flask(__name__)
 
