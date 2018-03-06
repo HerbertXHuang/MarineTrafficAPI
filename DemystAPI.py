fc45e645a984b64b6e6f00ce4658d3ee95f02b71
Herbert Huang <xh2305@columbia.edu>
2018-03-06 00:15:55 -0500

    New added

--- DemystAPI.py
+++ DemystAPI.py
@@ -2,16 +2,12 @@ from flask import Flask, render_template
 import json
 from static.MarineTraffic import MarineTraffic
 
-"""
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     
-    res = MarineTraffic().Search('YING XIANG')
+    res = MarineTraffic().Search('YING XIANG HONG')
 
     return render_template('index.html', joke = res)
-"""
-
-res = MarineTraffic().Search('YING XIANG')
-print(res)
