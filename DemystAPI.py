c4d133b6ab0c021b2f31097bc26e72e51b569ac1
Herbert Huang <xh2305@columbia.edu>
2018-03-06 19:12:57 -0500

    Finally

--- DemystAPI.py
+++ DemystAPI.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, redirect
 from static.MarineTraffic import MarineTraffic
 
 app = Flask(__name__)
@@ -6,17 +6,27 @@ app = Flask(__name__)
 def prettify(array):
 	string = list(map(lambda x:  '; '.join(x[0]) + '<br>' + '<br>'.join(x[1]), array))		
 	for i in range(len(string)):
-		string[i] = 'NO.' + str(i+1)+ ': ' + string[i]
-	return '<br>'.join(string)
+		string[i] = 'No.' + str(i+1)+ ': ' + string[i]
+	return '<br><br>'.join(string)
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-    if request.method == 'GET':
-    	return render_template('index.html')
+	if request.method == 'GET':
+		return render_template('index.html')
+	if request.method == 'POST':
+		keyword = request.form['keyword']
+		return redirect(url_for('search_page', keyword = keyword))
 
-    if request.method == 'POST':
-    	keyword = request.form['keyword']
-    	sum_res, res = MarineTraffic().Search(keyword)
-    	if len(res)>0:
-	    	res = prettify(res)
-    	return render_template('index.html', sum_res = sum_res, result = res)
+@app.route('/query', methods = ['GET', 'POST'])
+def search_page():
+	keyword = request.args.get('keyword')
+	sum_res, res = MarineTraffic().Search(keyword)
+	if len(res)>0:
+		res = prettify(res)
+	if request.method == 'GET':
+		return render_template('index.html', sum_res = sum_res, result = res)
+	if request.method == 'POST':
+		keyword = request.form['keyword']
+		return redirect(url_for('search_page', keyword = keyword))
+
+#app.run()
